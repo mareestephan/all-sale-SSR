969989268487715df6917ea9bf2b49e66516a029
mareestephan <stephanmaree@gmail.com>
2020-05-22 10:30:48 +0200

    slug changes made

--- components/categories.js
+++ components/categories.js
@@ -7,7 +7,7 @@ class Categories extends React.Component {
         const listItems = this.props.categories.pages_to_create.map((category) =>
 
             <div className="col-6 col-md one-quarter guide mt-0 pl-0 pl-sm-2 pr-1 pr-sm-2" key={category.category_id} >
-                <Link href='./clp/[category] ' as={category.slug} key={category.category_id} >
+                <Link href='./[category] ' as={category.slug} key={category.category_id} >
 
                     <a aria-label="product link" >
 
